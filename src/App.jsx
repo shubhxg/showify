04c6b736980fa984d40f084be4a436438df882f3
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-13 23:45:46 +0530

    update project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Card from "./Components/Card.jsx";
+import seriesData from "./data/seriesdata.json"
+
+export default function App() {
+  return (
+    <section className="main flex flex-col">
+      <header className="text-center max-w-100">
+        <h1 className="mt-8 text-3xl tracking-tighter font-bold">
+          Welcome to{" "}
+          <span className="bg-gradient-to-r from-violet-500 to-blue-500 bg-clip-text text-transparent animate-pulse">
+            Showify!
+          </span>
+        </h1>
+        <h6 className="mt-1 text-xs font-regular text-gray-300">
+          A place to find your next watch recommendation based on what you last
+          watched.
+        </h6>
+        <br />
+      </header>
+      <div className="card-container">
+        {seriesData.map((series) => {
+          return (
+            // passing the props to the card component
+            <Card
+              key={series.id}
+              rating={series.seriesRating}
+              imgSource={series.imgSource}
+              name={series.seriesName}
+              isNerdy={series.isNerdy}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+export default function Card({ imgSource, name, rating, isNerdy }) {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`Card been rendered!`);
+  }, [hasLiked]);
+
+  return (
+    <div
+      className="card py-3 px-3 ease-out duration-300 rounded-xl hover:bg-[#282346] hover:outline outline-violet-900 outline-2"
+      onClick={() => {
+        setCount((prevState) => prevState + 1);
+      }}
+    >
+      <img
+        className="object-cover mb-4 w-48 h-40 rounded-lg"
+        src={imgSource}
+        alt="Card"
+        width={"200px"}
+      />
+      <h3 className="text-xl mt-1 font-bold">{name}</h3>
+      <h5 className="text-sm ">⭐ {rating}</h5>
+      <div className="flex justify-between">
+        <h5 className="text-xs">
+          {isNerdy ? "Nerdy Show" : "Drama/Comedy"} {count || null}
+        </h5>
+        <button
+          onClick={() => setHasLiked((prevState) => !prevState)}
+          className={
+            hasLiked
+              ? "text-xs bg-violet-800 py-1 px-4 mt-2 rounded-md"
+              : "text-xs bg-[#212f4d] py-1 px-3 mt-2 rounded-md hover:bg-violet-900"
+          }
+        >
+          {hasLiked ? "💜 Liked" : "🤍"}
+        </button>
+      </div>
+    </div>
+  );
+}
