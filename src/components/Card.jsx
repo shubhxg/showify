9d1d45c3c8bfa20e8919213a03d5382851375f1c
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-14 08:10:01 +0530

    fix cards layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,10 +21,10 @@ export default function Card({ imgSource, name, rating, isNerdy }) {
         alt="Card"
         
       />
-      <h3 className="text-xl mt-1 font-bold">{name}</h3>
-      <h5 className="text-sm ">⭐ {rating}</h5>
+      <h3 className="text-2xl my-1 font-bold flex">{name}</h3>
+      <h5 className="text-md flex">⭐ {rating}</h5>
       <div className="flex justify-between">
-        <h5 className="text-xs">
+        <h5 className="text-md mt-2">
           {isNerdy ? "Nerdy Show" : "Drama/Comedy"} {count || null}
         </h5>
         <button
