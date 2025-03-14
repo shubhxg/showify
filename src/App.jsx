b0f6e2ea748806caebe000dc44210d5b79c36797
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-14 08:40:11 +0530

    ui improve

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import seriesData from "./data/seriesdata.json"
 export default function App() {
   return (
     <section>
-      <header className="text-center max-w-full">
-        <h1 className="mt-8 text-3xl tracking-tighter font-bold">
+      <header className="text-center max-w-full my-24">
+        <h1 className="mt-8 text-5xl tracking-tighter font-bold">
           Welcome to{" "}
           <span className="bg-gradient-to-r from-violet-500 to-blue-500 bg-clip-text text-transparent animate-pulse">
             Showify!
           </span>
         </h1>
-        <h6 className="mt-1 text-xs font-regular text-gray-300">
+        <h6 className="mt-4 text-lg font-regular text-gray-500">
           A place to find your next watch recommendation based on what you last
           watched.
         </h6>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,32 +10,31 @@ export default function Card({ imgSource, name, rating, isNerdy }) {
 
   return (
     <div
-      className="card py-3 px-3 ease-in-out duration-300 rounded-xl hover:bg-[#282346] hover:outline-violet-900 hover:outline-1"
+      className="cursor-pointer card py-3 px-3 ease-in-out duration-300 rounded-xl hover:bg-[#282346] hover:outline-violet-900 hover:outline-1"
       onClick={() => {
         setCount((prevState) => prevState + 1);
       }}
     >
       <img
-        className="object-cover mb-4 w-48 h-40 rounded-lg w-[300px] h-[250px]"
+        className="object-cover mb-4 rounded-lg w-[300px] h-[250px]"
         src={imgSource}
         alt="Card"
-        
       />
       <h3 className="text-2xl my-1 font-bold flex">{name}</h3>
       <h5 className="text-md flex">⭐ {rating}</h5>
       <div className="flex justify-between">
-        <h5 className="text-md mt-2">
+        <h5 className="text-md mt-2 text-gray-500">
           {isNerdy ? "Nerdy Show" : "Drama/Comedy"} {count || null}
         </h5>
         <button
           onClick={() => setHasLiked((prevState) => !prevState)}
           className={
             hasLiked
-              ? "text-xs bg-violet-800 py-1 px-4 mt-2 rounded-md"
-              : "text-xs bg-[#212f4d] py-1 px-3 mt-2 rounded-md hover:bg-violet-900"
+              ? "text-md ease-in-out duration-300 bg-violet-800 py-2 px-5 mt-2 rounded-md cursor-pointer"
+              : "text-md bg-[#38195c] py-2 px-5 mt-2 rounded-md ease-in-out duration-300 hover:bg-violet-900 cursor-pointer"
           }
         >
-          {hasLiked ? "💜 Liked" : "🤍"}
+          {hasLiked ? "❤️ Liked" : "🤍"}
         </button>
       </div>
     </div>
