10729fdbf13b55df7a773f3e63072be77707abd7
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-16 23:43:56 +0530

    add: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import Card from "./components/Card";
+import Header from "./components/Header";
 import seriesData from "./data/seriesdata.json"
 
 export default function App() {
   return (
     <section>
-      <header className="max-w-full py-8 flex flex-col items-center text-center">
-        <img src="/hero.png" alt="Hero Banner Image" className="sm:w-108 w-54" />
-        <h1 className="sm:mt-2 sm:text-5xl text-3xl tracking-tighter font-bold">
-          Welcome to{" "}
-          <span className="bg-gradient-to-r from-violet-500 to-red-400 bg-clip-text text-transparent animate-gradient-text">
-            Showify!
-          </span>
-        </h1>
-        <h6 className="mt-4 sm:text-lg text-md px-4 font-regular text-gray-500">
-          A place to find your next watch recommendation based on what you last
-          watched.
-        </h6>
-        <br />
-      </header>
+      <Header />
       <div className="card-container flex justify-center flex-wrap gap-8 max-w-full sm:mx-30">
         {seriesData.map((series) => {
           return (
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Search () {
+    return (
+        <div>
+            <input className="w-full rounded-lg bg-[#282346] p-2 text-white" type="text" placeholder="Search for a show..."></input>
+        </div>
+    )
+}
