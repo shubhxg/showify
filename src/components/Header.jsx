07ef0f016658b146125a320a54057e883e88ca9e
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-18 00:45:50 +0530

    ui improvements on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,13 @@ export default function Header() {
   return (
     <header className="max-w-full mt-24 flex flex-col items-center text-center">
       <img src="/hero.png" alt="Hero Banner Image" className="sm:w-92 w-54" />
-      <h1 className="sm:mt-2 sm:text-4xl text-3xl tracking-tighter font-bold">
-        Welcome to{" "}
+      <h1 className="sm:mt-2 sm:text-4xl text-2xl tracking-tighter font-bold sm:w-2xl sm:leading-12">
+        Find Your {""}
         <span className="bg-gradient-to-r from-violet-500 to-red-400 bg-clip-text text-transparent animate-gradient-text">
-          Showify!
+          Next Watch Recommendation{" "}
         </span>
+        based on what you Last Watched.{" "}
       </h1>
-      <h6 className="mt-2 sm:text-md px-4 font-regular text-gray-300">
-        Find your next watch recommendation based on what you last watched.
-      </h6>
       <br />
     </header>
   );
