6a4aac1bb991e695423cf55465c1a905d31839ee
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-18 22:45:29 +0530

    minor ui fixes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Search ({searchTerm, setSearchTerm}) {
     return (
-      <div className="sticky top-4 z-10 mb-24 sm:w-xl rounded-md bg-[#2f184a] px-4 flex justify-start items-center focus-within:ring-1 focus-within:ring-[#9e7bff]">
+      <div className="sticky top-4 z-10 mb-24 sm:w-xl rounded-full bg-[#241238] px-4 flex justify-start items-center focus-within:ring-1 focus-within:ring-[#9e7bff]">
         <div>
           <img src="/search.svg" className="w-4 mr-2" alt="search icon" />
         </div>
