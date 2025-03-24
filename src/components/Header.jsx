f0e1fbe60db7c173f2fee9e0fa58711893b7791e
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-03-25 00:09:00 +0530

    minor fixes for better text wrap and other fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="max-w-full mt-12 flex flex-col items-center text-center">
       <img src="/hero.png" alt="Hero Banner Image" className="sm:w-92 w-54" />
-      <h1 className="sm:mt-2 sm:text-4xl text-2xl tracking-tighter font-bold sm:w-2xl sm:leading-12">
+      <h1 className="sm:mt-2 sm:text-4xl text-2xl tracking-tighter font-bold sm:w-2xl sm:leading-12 sm:text-wrap-balance">
         Find Your {""}
         <span className="bg-gradient-to-r from-violet-500 to-red-400 bg-clip-text text-transparent animate-gradient-text">
           Next Watch Recommendation{" "}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Search ({searchTerm, setSearchTerm}) {
     return (
-      <div className="sticky top-4 z-10 mb-24 sm:w-xl rounded-full bg-[#291540]/80 backdrop-blur-sm outline-1 outline-[#543770] px-4 flex justify-start items-center focus-within:ring-2 focus-within:ring-[#9e7bff]">
+      <div className="sticky top-4 z-10 sm:mb-16 mb-8 sm:w-xl rounded-full bg-[#291540]/80 backdrop-blur-sm outline-1 outline-[#543770] px-4 flex justify-start items-center focus-within:ring-2 focus-within:ring-[#9e7bff]">
         <div>
           <img src="/search.svg" className="w-4 mr-2" alt="search icon" />
         </div>
         <input
           type="text"
-          placeholder="Search through thousands of tv shows and movies..."
-          className="sm:w-xl py-3 focus:outline-none"
+          placeholder="Search through thousands of movies..."
+          className="sm:w-xl w-54 py-2 sm:py-3 focus:outline-none"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
