88bee3238bfa6f7f960724c549c2a303392bb048
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-04-01 02:10:42 +0530

    style: update card styling and bg
    Adjust the background gradient in the section for a darker theme and add padding to the bottom. Simplify the Card component by removing the like button and improving the layout.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,32 @@
-import { useState } from "react";
 import { IMAGE_BASE_URL } from "../constants/constant";
 
 export default function Card({ movie: { poster_path, title, vote_average, original_language, release_date } }) {
-  const [hasLiked, setHasLiked] = useState(false);
-
   return (
-    <div className="text-white cursor-pointer p-4 duration-200 ease-in-out rounded-2xl bg-[#19142f] hover:bg-[#3a395a] outline outline-transparent hover:outline-violet-600 hover:scale-110 transform-gpu will-change-transform">
+    <div className=" relative text-white cursor-pointer relative duration-200 ease-in-out rounded-xl outline-4 outline-transparent hover:outline-violet-600 hover:scale-105 transform-gpu will-change-transform">
       <img
-        className="object-top object-cover mb-4 rounded-xl w-[280px] h-[250px]"
+        className="object-top object-cover rounded-xl w-[220px] h-full"
         src={poster_path ? `${IMAGE_BASE_URL}${poster_path}` : "/no-movie.png"}
         alt={title}
       />
-      <h3 className="sm:text-md my-1 font-bold flex">
-        {title.length > 27 ? `${title.substring(0, 27)}...` : title}
-      </h3>
-      <div className="flex items-center">
-        <h6 className="text-sm flex font-medium">
-          ⭐ {Number(vote_average).toFixed(1)}
-        </h6>
-        <span className="mx-1.5 text-gray-500">•</span>
-        <h6 className="text-sm flex text-gray-400">
-          {`${original_language[0].toUpperCase()}${original_language.slice(1)}`}
-        </h6>
-        <span className="mx-1.5 text-gray-500">•</span>
-        <h6 className="text-sm flex text-gray-400">
-          {`${release_date ? release_date.substring(0, 4) : "N/A"}`}
-        </h6>
-      </div>
-      <div className="flex justify-end">
-        <button
-          onClick={() => setHasLiked((prevState) => !prevState)}
-          className={
-            hasLiked
-              ? " bg-violet-700 py-2 px-4 mt-4 rounded-xl cursor-pointer"
-              : "bg-[#2d1549] py-2 px-4 mt-4 rounded-xl hover:bg-violet-700 cursor-pointer"
-          }
-        >
-          {hasLiked ? "👍 Liked" : "👍"}
-        </button>
+      <div className="absolute bottom-0 left-0 p-4 w-full bg-gradient-to-t from-black/90 to-transparent rounded-b-xl">
+        <h3 className="text-lg font-bold mb-2">
+          {title.length > 27 ? `${title.substring(0, 27)}...` : title}
+        </h3>
+        <div className="flex items-center gap-1.5 text-sm">
+          <span className="font-medium flex items-center">
+            ⭐ {Number(vote_average).toFixed(1)}
+          </span>
+          <span className="text-gray-400">•</span>
+          <span className="text-gray-300">
+            {`${original_language[0].toUpperCase()}${original_language.slice(
+              1
+            )}`}
+          </span>
+          <span className="text-gray-400">•</span>
+          <span className="text-gray-300">
+            {`${release_date ? release_date.substring(0, 4) : "N/A"}`}
+          </span>
+        </div>
       </div>
     </div>
   );
